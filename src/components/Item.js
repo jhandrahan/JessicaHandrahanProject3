56e55cc4de8ca196932c63abb364f5175b53af29
jhandrahan <jessicahandrahan@gmail.com>
2023-01-24 15:58:31 -0400

    fixed error

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,7 +7,7 @@ const Item = ({recipeData}) => {
 
             <div className="textContainer">
                 <h2>{recipeData.title}</h2>
-                <a className="link" href={recipeData.sourceUrl} target="_blank">View Recipe</a>
+                <a className="link" href={recipeData.sourceUrl}>View Recipe</a>
             </div>
         </li>
     )
